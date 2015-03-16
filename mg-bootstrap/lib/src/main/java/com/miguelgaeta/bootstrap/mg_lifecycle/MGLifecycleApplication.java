b16976ba1786f812144e5c66bfc7de8fdf1798f2
mrkcsc <miguel@miguelgaeta.com>
2015-03-16 00:40:07 -0700

    Bump version.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_lifecycle/MGLifecycleApplication.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_lifecycle/MGLifecycleApplication.java
@@ -2,10 +2,27 @@ package com.miguelgaeta.bootstrap.mg_lifecycle;
 
 import android.app.Application;
 
+import com.miguelgaeta.bootstrap.mg_preference.MGPreference;
+import com.miguelgaeta.bootstrap.mg_reflection.MGReflection;
+
 /**
  * Created by Miguel on 1/23/2015. Copyright 2014 Blitz Studios
  */
 @SuppressWarnings("UnusedDeclaration")
 public class MGLifecycleApplication extends Application {
 
+    /**
+     * Initialize libraries that require
+     * application level contexts.
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize reflection.
+        MGReflection.getConfig().init(this);
+
+        // Initialize preferences.
+        MGPreference.getConfig().init(this);
+    }
 }
