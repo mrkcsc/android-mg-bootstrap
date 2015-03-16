75b45ccaaa913188aba5e454f693c5ddcaffa129
mrkcsc <miguel@miguelgaeta.com>
2015-03-16 00:09:28 -0700

    Added reflective method for fetching build config files from anywhere using reflection.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_reflection/MGReflection.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_reflection/MGReflection.java
@@ -78,4 +78,26 @@ public class MGReflection {
 
         return point;
     }
+
+    /**
+     * Gets a field from the project's BuildConfig. This is useful when, for example, flavors
+     * are used at the project level to set custom fields.
+     * @param context Used to find the correct file
+     * @param fieldName The name of the field-to-access
+     * @return The value of the field, or {@code null} if the field is not found.
+     */
+    public static Object getBuildConfigValue(Context context, String fieldName) {
+        try {
+
+            // Fetch app build config class.
+            Class<?> clazz = Class.forName(context.getPackageName() + ".BuildConfig");
+
+            // Fetch associated field.
+            return clazz.getField(fieldName).get(null);
+
+        } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+
+            return null;
+        }
+    }
 }
