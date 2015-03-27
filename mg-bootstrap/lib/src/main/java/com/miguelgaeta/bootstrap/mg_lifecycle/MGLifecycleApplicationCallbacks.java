78d2c1c16f1530f0ed9ed19285df4c6c21b81a0f
mrkcsc <miguel@miguelgaeta.com>
2015-03-27 02:15:51 -0700

    Added convenience class for application lifecycle callbacks.

---
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_lifecycle/MGLifecycleApplicationCallbacks.java
@@ -0,0 +1,32 @@
+package com.miguelgaeta.bootstrap.mg_lifecycle;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Created by mrkcsc on 3/27/15.
+ */
+public class MGLifecycleApplicationCallbacks implements Application.ActivityLifecycleCallbacks {
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle bundle) { }
+
+    @Override
+    public void onActivityStarted(Activity activity) { }
+
+    @Override
+    public void onActivityResumed(Activity activity) { }
+
+    @Override
+    public void onActivityPaused(Activity activity) { }
+
+    @Override
+    public void onActivityStopped(Activity activity) { }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) { }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) { }
+}
