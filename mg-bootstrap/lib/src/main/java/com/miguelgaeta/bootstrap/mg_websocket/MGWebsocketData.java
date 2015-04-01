fc129a57e72f982e4945d555e8f582a9409aab92
mrkcsc <miguel@miguelgaeta.com>
2015-04-01 02:20:56 -0700

    Added full json serialization support, bumped to 1.4.0

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_websocket/MGWebsocketData.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_websocket/MGWebsocketData.java
@@ -1,20 +1,25 @@
 package com.miguelgaeta.bootstrap.mg_websocket;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import rx.Observable;
 
 /**
  * Created by mrkcsc on 3/11/15.
  */
+@SuppressWarnings("UnusedDeclaration")
 public class MGWebsocketData {
 
     // Private constructor.
     private MGWebsocketData() { }
 
-    @Data @AllArgsConstructor(staticName = "create")
+    @AllArgsConstructor(staticName = "create") @Getter @ToString @EqualsAndHashCode
     public static class Open {
 
         // Http status code.
@@ -24,7 +29,7 @@ public class MGWebsocketData {
         private String httpStatusMessage;
     }
 
-    @Data @AllArgsConstructor(staticName = "create")
+    @AllArgsConstructor(staticName = "create") @Getter @ToString @EqualsAndHashCode
     public static class Closed {
 
         // Status code.
@@ -37,22 +42,40 @@ public class MGWebsocketData {
         private boolean remote;
     }
 
-    @Data @AllArgsConstructor(staticName = "create")
+    @AllArgsConstructor(staticName = "create") @Getter @ToString @EqualsAndHashCode
     public static class Message {
 
         // Raw message.
         private String message;
 
         /**
-         * Fetch as a json element.
+         * Fetch the message string
+         * as a json element.
          */
-        public JsonElement getMessageJson() {
+        public Observable<JsonElement> getMessageJson() {
 
-            return new JsonParser().parse(message);
+            return Observable.create(subscriber -> {
+
+                subscriber.onNext(new JsonParser().parse(message));
+                subscriber.onCompleted();
+            });
+        }
+
+        /**
+         * Fetch the message string
+         * as a serialized JSON object.
+         */
+        public <T> Observable<T> getMessageJson(Class<T> classOfT) {
+
+            return Observable.create(subscriber -> {
+
+                subscriber.onNext(new Gson().fromJson(message, classOfT));
+                subscriber.onCompleted();
+            });
         }
     }
 
-    @Data @AllArgsConstructor(staticName = "create")
+    @AllArgsConstructor(staticName = "create") @Getter @ToString @EqualsAndHashCode
     public static class Error {
 
         // Exception.
