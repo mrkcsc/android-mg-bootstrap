18afc0cfb38882a5e384b44085a134f57759aa09
Miguel Gaeta <miguel@miguelgaeta.com>
2015-04-10 15:15:00 -0700

    Added custom text view.

---
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_view/MGViewText.java
@@ -0,0 +1,40 @@
+package com.miguelgaeta.bootstrap.mg_view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Miguel Gaeta on 4/10/15.
+ */
+public class MGViewText extends TextView {
+
+    public MGViewText(Context context) {
+        super(context);
+    }
+
+    public MGViewText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MGViewText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * Fix some probably undesired behavior in android where if
+     * there is no text the view still has a height.
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        if (getText() == null || getText().length() == 0) {
+
+            heightMeasureSpec = View.MeasureSpec
+                .makeMeasureSpec(0, View.MeasureSpec.getMode(heightMeasureSpec));
+        }
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
