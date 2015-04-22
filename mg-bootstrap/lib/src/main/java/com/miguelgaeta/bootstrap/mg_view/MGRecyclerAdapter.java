9c1f4f5721ce67f6af4d28cd254a0b66b4930a32
Miguel Gaeta <miguel@miguelgaeta.com>
2015-04-21 20:10:02 -0700

    Introduced lifecycle methods to the custom recycler view and adapter callbacks.
    Bumped to 1.8.4

---
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_view/MGRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.miguelgaeta.bootstrap.mg_view;
+
+import android.support.v7.widget.RecyclerView;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import rx.Observable;
+
+/**
+ * Created by Miguel Gaeta on 4/21/15.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public abstract class MGRecyclerAdapter extends RecyclerView.Adapter<MGViewRecyclerHolder> {
+
+    @Getter(AccessLevel.PROTECTED)
+    private RecyclerView recycler;
+
+    @Getter(AccessLevel.PROTECTED)
+    private final Observable<Void> paused;
+
+    /**
+     * This adapter streamlines common recycler view operations
+     * and boiler plate code.  In addition it exposts lifecycle
+     * methods to the view holder cells which allow for
+     * much easier mutation.
+     */
+    public MGRecyclerAdapter(@NonNull RecyclerView recycler, @NonNull Observable<Void> paused) {
+
+        // Set recycler view.
+        this.recycler = recycler;
+
+        // Set on paused event.
+        this.paused = paused;
+    }
+
+    /**
+     * Invoke the lifecycle resume callback
+     * on the associated view holder.
+     */
+    @Override
+    public void onViewAttachedToWindow(MGViewRecyclerHolder holder) {
+        super.onViewAttachedToWindow(holder);
+
+        holder.onResume();
+    }
+
+    /**
+     * Invoke the lifecycle pause callback
+     * on the associated view holder.
+     */
+    @Override
+    public void onViewDetachedFromWindow(MGViewRecyclerHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+
+        holder.onPause();
+    }
+
+    /**
+     * Automatically configure the view holder
+     * with the specified item at position.
+     */
+    @Override
+    public void onBindViewHolder(MGViewRecyclerHolder holder, int position) {
+
+        holder.configure(position);
+    }
+}
--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_view/MGViewRecyclerHolder.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_view/MGViewRecyclerHolder.java
@@ -4,30 +4,79 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import butterknife.ButterKnife;
+import lombok.AccessLevel;
 import lombok.Getter;
+import rx.Observable;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by Miguel Gaeta on 4/9/15.
  */
-@SuppressWarnings({"unused", "FieldCanBeLocal"})
-public abstract class MGViewRecyclerHolder<T extends RecyclerView.Adapter> extends RecyclerView.ViewHolder {
+@SuppressWarnings({"unused", "FieldCanBeLocal" })
+public abstract class MGViewRecyclerHolder<T extends MGRecyclerAdapter> extends RecyclerView.ViewHolder {
 
-    @Getter
-    private T adapter;
+    @Getter(AccessLevel.PROTECTED)
+    private final T adapter;
 
-    private MGViewRecyclerHolder(View itemView) {
-        super(itemView);
-    }
+    @Getter(AccessLevel.PRIVATE)
+    private final PublishSubject<Void> pausedHolder = PublishSubject.create();
 
+    @Getter(AccessLevel.PRIVATE)
+    private final Observable<Void> pausedAdapter;
+
+    /**
+     * This recycler view holder subclass exposes
+     * the associated adapter so it can self
+     * configure itself.
+     */
     public MGViewRecyclerHolder(View itemView, T adapter) {
         super(itemView);
 
         // Set the adapter.
         this.adapter = adapter;
 
+        // Set adapter paused observable.
+        this.pausedAdapter = adapter.getPaused();
+
         // Enable butter knife.
         ButterKnife.inject(this, itemView);
     }
 
+    /**
+     * Simulates the on resume method of
+     * fragments and activities.
+     */
+    protected void onResume() {
+
+        // Emit pause if the adapter paused.
+        getPausedAdapter().takeUntil(getPaused()).subscribe(r -> {
+
+            getPausedHolder().onNext(null);
+        });
+    }
+
+    /**
+     * Simulates the on pause method of
+     * fragments and activities.
+     */
+    protected void onPause() {
+
+        // Or if paused called directly.
+        getPausedHolder().onNext(null);
+    }
+
+    /**
+     * Gets the paused stream as
+     * an observable.
+     */
+    protected final Observable<Void> getPaused() {
+
+        return getPausedHolder().asObservable();
+    }
+
+    /**
+     * Configuration method for the view holder.  This method
+     * is enforced for easy visibility.
+     */
     public abstract void configure(int position);
 }
