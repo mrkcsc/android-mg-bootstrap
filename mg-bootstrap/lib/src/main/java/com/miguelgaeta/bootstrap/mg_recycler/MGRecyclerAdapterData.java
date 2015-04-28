678281e6a6c5fc3e2842fec9306a5a0835a23c5c
Miguel Gaeta <miguel@miguelgaeta.com>
2015-04-28 13:42:08 -0700

    Added adapter helper class,  bumped to 1.9.7

---
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_recycler/MGRecyclerAdapterData.java
@@ -0,0 +1,63 @@
+package com.miguelgaeta.bootstrap.mg_recycler;
+
+import lombok.NonNull;
+
+/**
+ * Created by Miguel Gaeta on 4/28/15.
+ */
+public class MGRecyclerAdapterData<T> {
+
+    private T data;
+
+    @NonNull
+    private DataUpdated<T> dataUpdated;
+
+    /**
+     * Private constructor.
+     */
+    private MGRecyclerAdapterData() {
+
+    }
+
+    /**
+     * Static constructor.
+     */
+    public static <T> MGRecyclerAdapterData<T> create(T initialData, DataUpdated<T> dataUpdated) {
+
+        MGRecyclerAdapterData<T> data = new MGRecyclerAdapterData<>();
+
+        data.data = initialData;
+        data.dataUpdated = dataUpdated;
+
+        return data;
+    }
+
+    public T getData() {
+
+        return data;
+    }
+
+    /**
+     * If data changed, set new data, store
+     * old data and emit callback.
+     */
+    public void setData(T data) {
+
+        if (data == null ? this.data != null : !data.equals(this.data)) {
+
+            T oldData = this.data;
+
+            this.data = data;
+
+            dataUpdated.updated(oldData, this.data);
+        }
+    }
+
+    /**
+     * Callback interface.
+     */
+    public interface DataUpdated<T> {
+
+        void updated(T oldData, T newData);
+    }
+}
