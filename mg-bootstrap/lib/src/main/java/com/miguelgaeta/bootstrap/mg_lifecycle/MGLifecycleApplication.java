2a779efd85b64ad3f8f2fdc3990e62a4303e0487
Miguel Gaeta <miguel@miguelgaeta.com>
2015-05-04 16:03:55 -0700

    Dummy proof the lifecycle context.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_lifecycle/MGLifecycleApplication.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_lifecycle/MGLifecycleApplication.java
@@ -3,14 +3,11 @@ package com.miguelgaeta.bootstrap.mg_lifecycle;
 import android.app.Application;
 import android.content.Context;
 
-import lombok.Getter;
-
 /**
  * Created by Miguel Gaeta on 5/4/15.
  */
 public class MGLifecycleApplication extends Application {
 
-    @Getter
     private static Context context;
 
     @Override
@@ -18,5 +15,20 @@ public class MGLifecycleApplication extends Application {
         super.onCreate();
 
         context = this;
+    }
+
+    /**
+     * Fetch the application context. Should
+     * be used with care.
+     */
+    public static Context getContext() {
+
+        if (context == null) {
+
+            // This should only ever happen if the consumer forgot to subclass.
+            throw new RuntimeException("Application context unavailable.  Did you remember to subclass MGLifecycleApplication?");
+        }
+
+        return context;
     }
 }
