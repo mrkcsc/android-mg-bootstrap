5500a97feea9f68cbe7cc6884360ffd0e351d688
mrkcsc <miguel@miguelgaeta.com>
2015-09-25 14:10:17 -0700

    Added kryo serializer code.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_preference/MGPrefStoreTypeByeStream.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_preference/MGPrefStoreTypeByeStream.java
@@ -3,7 +3,10 @@ package com.miguelgaeta.bootstrap.mg_preference;
 import android.content.Context;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+import com.miguelgaeta.bootstrap.mg_log.MGLog;
 
+import java.io.FileNotFoundException;
 import java.lang.reflect.Type;
 
 import lombok.Getter;
@@ -13,6 +16,8 @@ import lombok.NonNull;
  * Created by Miguel Gaeta on 9/25/15.
  */
 public class MGPrefStoreTypeByeStream implements MGPrefStoreInterface {
+
+    private Context context;
 
     @Getter(lazy = true)
     private final ThreadLocal<Kryo> kryos = new ThreadLocal<Kryo>() {
@@ -26,10 +31,13 @@ public class MGPrefStoreTypeByeStream implements MGPrefStoreInterface {
     @Override
     public void init(@NonNull Context context) {
 
+        this.context = context;
     }
 
     @Override
     public Object get(@NonNull String key, Type typeOfObject, boolean versioned) {
+
+        // TODO
 
         return null;
     }
@@ -37,10 +45,23 @@ public class MGPrefStoreTypeByeStream implements MGPrefStoreInterface {
     @Override
     public void set(@NonNull String key, Object value, Type typeOfObject, boolean versioned) {
 
+        try {
+            final Output output =
+                new Output(context.openFileOutput("shared_prefs_key.bin", Context.MODE_PRIVATE));
+
+            getKryos().get().writeClassAndObject(output, value);
+
+            output.close();
+
+        } catch (FileNotFoundException e) {
+
+            MGLog.i(e, "File not found for key: " + key);
+        }
     }
 
     @Override
     public void clear() {
 
+        // TODO
     }
 }
