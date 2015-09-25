0264817436bd4339b33b6c766ca7d67fd6b21b2a
mrkcsc <miguel@miguelgaeta.com>
2015-09-25 13:47:48 -0700

    Add kryo thread pool instance.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_preference/MGPrefStoreTypeByeStream.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_preference/MGPrefStoreTypeByeStream.java
@@ -2,14 +2,26 @@ package com.miguelgaeta.bootstrap.mg_preference;
 
 import android.content.Context;
 
+import com.esotericsoftware.kryo.Kryo;
+
 import java.lang.reflect.Type;
 
+import lombok.Getter;
 import lombok.NonNull;
 
 /**
  * Created by Miguel Gaeta on 9/25/15.
  */
 public class MGPrefStoreTypeByeStream implements MGPrefStoreInterface {
+
+    @Getter(lazy = true)
+    private final ThreadLocal<Kryo> kryos = new ThreadLocal<Kryo>() {
+
+        protected Kryo initialValue() {
+
+            return new Kryo();
+        }
+    };
 
     @Override
     public void init(@NonNull Context context) {
@@ -18,6 +30,7 @@ public class MGPrefStoreTypeByeStream implements MGPrefStoreInterface {
 
     @Override
     public Object get(@NonNull String key, Type typeOfObject, boolean versioned) {
+
         return null;
     }
 
