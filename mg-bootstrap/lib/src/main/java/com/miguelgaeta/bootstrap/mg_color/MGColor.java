ba8f6437c6c72bc4294760aed8d6c096a803932e
mrkcsc <miguel@miguelgaeta.com>
2015-10-12 17:13:49 -0700

    Pass in res id.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_color/MGColor.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_color/MGColor.java
@@ -2,11 +2,10 @@ package com.miguelgaeta.bootstrap.mg_color;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.support.annotation.AttrRes;
 import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
-
-import com.miguelgaeta.bootstrap.R;
 
 import lombok.NonNull;
 
@@ -20,13 +19,13 @@ public class MGColor {
         return ContextCompat.getColor(context, colorResourceId);
     }
 
-    public static int fromTheme(@NonNull Context context) {
+    public static int fromTheme(@NonNull Context context, @AttrRes int attributeResourceId) {
 
         final TypedValue typedValue = new TypedValue();
 
         final Resources.Theme theme = context.getTheme();
 
-        theme.resolveAttribute(R.attr.colorPrimaryDark, typedValue, true);
+        theme.resolveAttribute(attributeResourceId, typedValue, true);
 
         return typedValue.data;
     }
