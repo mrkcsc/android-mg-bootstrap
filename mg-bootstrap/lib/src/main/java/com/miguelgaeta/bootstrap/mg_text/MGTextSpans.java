e45be9fd532580786e5bf667fe567017c861eac1
mrkcsc <miguel@miguelgaeta.com>
2015-10-25 23:50:51 -0700

    Added monospace, and relative size spans.  Adjusted API.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_text/MGTextSpans.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_text/MGTextSpans.java
@@ -3,16 +3,17 @@ package com.miguelgaeta.bootstrap.mg_text;
 import android.graphics.Typeface;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextPaint;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.CharacterStyle;
 import android.text.style.ClickableSpan;
+import android.text.style.RelativeSizeSpan;
 import android.text.style.StrikethroughSpan;
 import android.text.style.StyleSpan;
+import android.text.style.TypefaceSpan;
 import android.text.style.UnderlineSpan;
 import android.view.View;
-
-import rx.functions.Action1;
 
 /**
  * Created by Miguel Gaeta on 7/14/15.
@@ -20,47 +21,22 @@ import rx.functions.Action1;
 @SuppressWarnings("UnusedDeclaration")
 public class MGTextSpans {
 
-    public enum Type {
-        BOLD, BOLD_ITALIC, ITALIC, UNDERLINE, STRIKE_THROUGH
+    public static CharacterStyle getMonospace() {
+        return new TypefaceSpan("monospace");
     }
 
-    public static CharacterStyle get(Type type) {
-
-        switch (type) {
-
-            case BOLD:
-
-                return new StyleSpan(Typeface.BOLD);
-
-            case BOLD_ITALIC:
-
-                return new StyleSpan(Typeface.BOLD_ITALIC);
-
-            case ITALIC:
-
-                return new StyleSpan(Typeface.ITALIC);
-
-            case STRIKE_THROUGH:
-
-                return new StrikethroughSpan();
-
-            case UNDERLINE:
-
-                return new UnderlineSpan();
-        }
-
-        throw new RuntimeException("Unrecognized type.");
+    public static CharacterStyle getRelativeSize(float proportion) {
+        return new RelativeSizeSpan(proportion);
     }
 
-    public static CharacterStyle getClickable(Action1<View> onClick, @ColorInt int color, boolean underline) {
-
+    public static CharacterStyle getClickable(@ColorInt int color, boolean underline, @Nullable View.OnClickListener onClick) {
         return new ClickableSpan() {
 
             @Override
             public void onClick(View view) {
 
                 if (onClick != null) {
-                    onClick.call(view);
+                    onClick.onClick(view);
                 }
             }
 
@@ -73,8 +49,27 @@ public class MGTextSpans {
         };
     }
 
-    public static CharacterStyle getBackgroundColor(@ColorInt int color) {
+    public static CharacterStyle getBold() {
+        return new StyleSpan(Typeface.BOLD);
+    }
 
+    public static CharacterStyle getBoldItalic() {
+        return new StyleSpan(Typeface.BOLD_ITALIC);
+    }
+
+    public static CharacterStyle getItalic() {
+        return new StyleSpan(Typeface.ITALIC);
+    }
+
+    public static CharacterStyle getStrikethrough() {
+        return new StrikethroughSpan();
+    }
+
+    public static CharacterStyle getUnderline() {
+        return new UnderlineSpan();
+    }
+
+    public static CharacterStyle getBackgroundColor(@ColorInt int color) {
         return new BackgroundColorSpan(color);
     }
 }
