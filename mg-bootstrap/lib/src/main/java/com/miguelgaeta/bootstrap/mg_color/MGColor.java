57119994e64515f4658100da98ec874b4e409e85
mrkcsc <miguel@miguelgaeta.com>
2015-12-04 13:01:34 -0800

    Add return type annotations.

--- mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_color/MGColor.java
+++ mg-bootstrap/lib/src/main/java/com/miguelgaeta/bootstrap/mg_color/MGColor.java
@@ -3,6 +3,7 @@ package com.miguelgaeta.bootstrap.mg_color;
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.AttrRes;
+import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
@@ -14,12 +15,14 @@ import lombok.NonNull;
  */
 public class MGColor {
 
-    public static int fromResource(@NonNull Context context, @ColorRes int colorResourceId) {
+    @SuppressWarnings("unused")
+    public static @ColorInt int fromResource(@NonNull Context context, @ColorRes int colorResourceId) {
 
         return ContextCompat.getColor(context, colorResourceId);
     }
 
-    public static int fromTheme(@NonNull Context context, @AttrRes int attributeResourceId) {
+    @SuppressWarnings("unused")
+    public static @ColorInt int fromTheme(@NonNull Context context, @AttrRes int attributeResourceId) {
 
         final TypedValue typedValue = new TypedValue();
 
